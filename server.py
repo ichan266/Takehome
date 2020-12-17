89b952d4b8788dafbee11dbbff312e4a1348b4cf
Iris Kuhn <ichan266@hotmail.com>
2020-12-17 07:18:36 +0000

    first commit-Firebase SKD links in html

---
+++ server.py
@@ -0,0 +1,24 @@
+"""Server for Takehome Challenge."""
+
+from flask import (Flask, render_template, request, flash, session,
+                   redirect, url_for, jsonify)
+from jinja2 import StrictUndefined
+import os
+
+app = Flask(__name__)
+if "SECRET_KEY" in os.environ:
+    app.secret_key = os.environ["SECRET_KEY"]
+else:
+    app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route("/")
+def homepage():
+    """Render homepage."""
+
+    return render_template("homepage.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
