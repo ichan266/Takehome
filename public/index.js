84282608a162fbd2cd241747425db5fa70a8a91d
Iris Kuhn <ichan266@hotmail.com>
2020-12-26 17:36:44 -0800

    Firebase CLI working (no vagrant)

---
+++ public/index.js
@@ -0,0 +1,13 @@
+console.log(firebase)
+
+// var config = {
+//   apiKey: "AIzaSyB32Zo9bZBux_8-CZdGuETzKbEr7aJxfaY",
+//   authDomain: "takehome-challenge-12-20.firebaseapp.com",
+//   databaseURL: "https://takehome-challenge-12-20-default-rtdb.firebaseio.com/",
+//   storageBucket: "takehome-challenge-12-20.appspot.com"
+// };
+
+// firebase.initializeApp(config);
+
+// // Get a reference to the database service
+// var database = firebase.database();
